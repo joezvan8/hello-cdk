3bc98761109ff901f0afa53c2d26b0ba6ce8e5a7
joezvan8 <jovan.clue@bison.howard.edu>
2025-11-16 16:25:25 -0500

    Changes to be committed: 	modified:   bin/hello-cdk.ts 	modified:   lib/hello-cdk-stack.ts 	new file:   package-lock.json

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib/core';
 import { HelloCdkStack } from '../lib/hello-cdk-stack';
 
@@ -14,7 +15,7 @@ new HelloCdkStack(app, 'HelloCdkStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+   env: { account: '677276102333', region: 'us-east-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,17 +1,26 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lb/aws-lambda';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+   // Define the Lambda function resource
+    const myFunction = new lambda.Function(this, "HelloWorldFunction",
+            {        
+            runtime: lambda.Runtime.NODEJS_20_X,
+            handler: "index.handler",
+            code: lambda.Code.fromInline(`
+                 exports.handler = async function(event) {
+                      return {
+                              statusCode: 200,
+                              body: JSON.stringify('Hello World!'),
+                      };
+                };              
+              `),
+            });
   }
 }
+
